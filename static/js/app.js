38e8a389538efef22789b7ae8297c2ac889cee47
Payson <pscush@gmail.com>
2023-06-20 21:18:34 -0700

    pretty clean

--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,58 @@
 const DATA_URL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Initialize the dashboard at start up 
 function init() {
-  d3.json(DATA_URL).then(function(data) {
-
-    // Set the initial selected sample ID as the first subject ID
-    firstSampleid = data.names[0];
-
-    // Build the dropdown options
-    const dropdown = d3.select("#selDataset");
-    dropdown.selectAll("option").remove();
-    data.names.forEach(id => {
-      dropdown
-        .append("option")
-        .attr("value", id)
-        .text(`Sample ${id}`);
+    d3.json(DATA_URL).then(function (data) {
+      // Set the initial selected sample ID as the first subject ID
+      const firstSampleid = data.names[0];
+  
+      // Build the dropdown options
+      const dropdown = d3.select("#selDataset");
+    //   dropdown.selectAll("option").remove();
+      data.names.forEach((id) => {
+        dropdown
+          .append("option")
+          .attr("value", id)
+          .text(`Sample ${id}`);
+      });
+  
+      // Call the optionChanged function with the initial sample ID
+      optionChanged(firstSampleid);
     });
-
-    // Build the initial plots
-    drawMetadata(firstSampleid);
-    drawBarChart(firstSampleid);
-    drawBubbleChart(firstSampleid);
-    drawGaugeChart(firstSampleid);
-  });
-}
-
-function drawMetadata(sample) {
-  // Use D3 to retrieve all of the data
-  d3.json(DATA_URL).then((data) => {
+  }
+  
+  function drawMetadata(data, sample) {
     d3.select("#sample-metadata").html("");
-    const selectedMetadata = data.metadata.filter(md => md.id == sample)[0];
-
+    const selectedMetadata = data.metadata.filter((md) => md.id == sample)[0];
+  
     // Use Object.entries to add each key/value pair to the panel
     Object.entries(selectedMetadata).forEach(([key, value]) => {
-      d3.select("#sample-metadata").append("h5").text(`${key}: ${value}`);
+      d3.select("#sample-metadata")
+        .append("h5")
+        .text(`${key}: ${value}`);
     });
-  });
-}
-
-function drawBarChart(sample) {
-  // Use D3 to retrieve all of the data
-  d3.json(DATA_URL).then((data) => {
-    const selectedSample = data.samples.filter(sd => sd.id == sample)[0];
+  }
+  
+  function drawBarChart(data, sample) {
+    const selectedSample = data.samples.filter((sd) => sd.id == sample)[0];
     const top10Samplevalues = selectedSample.sample_values.slice(0, 10).reverse();
     const top10OtuIDs = selectedSample.otu_ids.slice(0, 10).reverse();
     const top10Otulabels = selectedSample.otu_labels.slice(0, 10).reverse();
-
+  
     // Create the trace for the bar chart
     const trace1 = {
       x: top10Samplevalues,
-      y: top10OtuIDs.map(id => `OTU ${id}`),
+      y: top10OtuIDs.map((id) => `OTU ${id}`),
       text: top10Otulabels,
       type: "bar",
       orientation: "h",
       marker: {
         color: "#d3ca0e",
-      }
+      },
     };
-
+  
     // Create the data array for the bar chart
     const barData = [trace1];
-
+  
     // Set the layout for the bar chart
     const layout1 = {
       title: "Top 10 OTUs",
@@ -69,54 +61,52 @@ function drawBarChart(sample) {
       width: 450,
       height: 460,
     };
-
+  
     // Plot the bar chart
     Plotly.newPlot("bar", barData, layout1);
-  });
-}
-
-function drawBubbleChart(sample) {
-  // Use D3 to retrieve all of the data
-  d3.json(DATA_URL).then((data) => {
-    const selectedSample = data.samples.filter(sd => sd.id == sample)[0];
-
+  }
+  
+  function drawBubbleChart(data, sample) {
+    const selectedSample = data.samples.filter((sd) => sd.id == sample)[0];
+  
     // Create the trace for the bubble chart
     const trace2 = {
       x: selectedSample.otu_ids,
       y: selectedSample.sample_values,
       text: selectedSample.otu_labels,
-      mode: 'markers',
+      mode: "markers",
       marker: {
         size: selectedSample.sample_values,
         color: selectedSample.otu_ids,
-        colorscale: 'Earth'
-      }
+        colorscale: "Earth",
+      },
     };
-
+  
     // Create the data array for the bubble chart
     const bubbleData = [trace2];
-
+  
     // Set the layout for the bubble chart
     const bubbleLayout = {
-      title: 'Sample Values vs. OTU IDs',
-      xaxis: { title: 'OTU IDs' },
-      yaxis: { title: 'Sample Values' },
-      showlegend: false
+      title: "Sample Values vs. OTU IDs",
+      xaxis: { title: "OTU IDs" },
+      yaxis: { title: "Sample Values" },
+      showlegend: false,
     };
-
+  
     // Plot the bubble chart
-    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
-  });
-}
-
-// Function that updates dashboard when sample is changed
-function optionChanged(selectedSample) {
-  // Call all functions
-  drawMetadata(selectedSample);
-  drawBarChart(selectedSample);
-  drawBubbleChart(selectedSample);
-  drawGaugeChart(selectedSample);
-}
-
-// Call the initialize function
-init();
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  }
+  
+  // Function that updates dashboard when sample is changed
+  function optionChanged(selectedSample) {
+    // Load the data and update the dashboard
+    d3.json(DATA_URL).then((data) => {
+      drawMetadata(data, selectedSample);
+      drawBarChart(data, selectedSample);
+      drawBubbleChart(data, selectedSample);
+      drawGaugeChart(data, selectedSample);
+    });
+  }
+  
+  // Call the initialize function
+  init();
--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,48 +1,50 @@
 console.log("Bonus script loaded.");
 
-function drawGaugeChart(sample) {
+function drawGaugeChart(data, sample) {
   // Use D3 to retrieve all of the data
-  d3.json(DATA_URL).then((data) => {
-    const selectedMetadata = data.metadata.filter(md => md.id == sample)[0];
-    let washValue = selectedMetadata.wfreq;
+  const selectedMetadata = data.metadata.filter(md => md.id == sample)[0];
+  let washValue = selectedMetadata.wfreq;
 
-    // Create the trace for the gauge chart
-    const trace3 = {
-      type: "indicator",
-      mode: "gauge+number",
-      value: washValue,
-      title: { text: "Weekly Washing Frequency" },
-      gauge: {
-        axis: { range: [0, 10] },
-        steps: [
-          { range: [0, 1], color: "rgb(97, 76, 57)" },
-          { range: [1, 2], color: "rgb(115, 92, 69)" },
-          { range: [2, 3], color: "rgb(133, 108, 82)" },
-          { range: [3, 4], color: "rgb(161, 134, 63)" },
-          { range: [4, 5], color: "rgb(189, 160, 43)" },
-          { range: [5, 6], color: "rgb(217, 187, 24)" },
-          { range: [6, 7], color: "rgb(162, 193, 10)" },
-          { range: [7, 8], color: "rgb(107, 199, 0)" },
-          { range: [8, 9], color: "rgb(42, 205, 0)" },
-          { range: [9, 10], color: "rgb(0, 211, 0)" }
-        ],
-        bar: { color: "#066d92" },
-        borderwidth: 2,
-        bordercolor: "black",
+  // Create the trace for the gauge chart
+  const trace3 = {
+    type: "indicator",
+    mode: "gauge+number",
+    value: washValue,
+    title: { text: "Weekly Washing Frequency" },
+    gauge: {
+      axis: {
+        range: [0, 10],
+        tickvals: [0, 2, 4, 6, 8, 10],
+        ticktext: ["0", "2", "4", "6", "8", "10"],
       },
-    };
+      steps: [
+        { range: [0, 1], color: "rgb(97, 76, 57)" },
+        { range: [1, 2], color: "rgb(115, 92, 69)" },
+        { range: [2, 3], color: "rgb(133, 108, 82)" },
+        { range: [3, 4], color: "rgb(161, 134, 63)" },
+        { range: [4, 5], color: "rgb(189, 160, 43)" },
+        { range: [5, 6], color: "rgb(217, 187, 24)" },
+        { range: [6, 7], color: "rgb(162, 193, 10)" },
+        { range: [7, 8], color: "rgb(107, 199, 0)" },
+        { range: [8, 9], color: "rgb(42, 205, 0)" },
+        { range: [9, 10], color: "rgb(0, 211, 0)" }
+      ],
+      bar: { color: "#066d92" },
+      borderwidth: 2,
+      bordercolor: "black",
+    },
+  };
 
-    // Create the data array for the gauge chart
-    const gaugeData = [trace3];
+  // Create the data array for the gauge chart
+  const gaugeData = [trace3];
 
-    // Set the layout for the gauge chart
-    const gaugeLayout = {
-      width: 450,
-      height: 460,
-      margin: { t: 0, b: 0 },
-    };
+  // Set the layout for the gauge chart
+  const gaugeLayout = {
+    width: 450,
+    height: 460,
+    margin: { t: 0, b: 0 },
+  };
 
-    // Plot the gauge chart
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
-  });
+  // Plot the gauge chart
+  Plotly.newPlot("gauge", gaugeData, gaugeLayout);
 }
